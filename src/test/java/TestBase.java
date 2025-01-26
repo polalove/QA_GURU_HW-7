459f449609a1a66bc9ad94379ec32dfd02e9f0e5
Ivan Novikov <pola2288@yandex.ru>
2025-01-26 20:07:08 +0300

    fix video on Allure

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -19,11 +19,13 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 10000;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
+
         Configuration.browserCapabilities = capabilities;
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
