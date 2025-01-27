d9d9924799cd8a99c3a3cb9d614a218258d742c5
Ivan Main PC <pola2288@yandex.ru>
2025-01-27 22:52:21 +0300

    fix TestBase

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -16,11 +16,11 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = System.getProperty("browser");
+        Configuration.browser = System.getProperty("browserType");
         Configuration.browserVersion = System.getProperty("browserVersion");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@" + System.getProperty("wdHost") +"/wd/hub";
+        Configuration.remote = "https://user1:1234@" + System.getProperty("remoteHost") +"/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
@@ -29,8 +29,6 @@ public class TestBase {
         ));
 
         Configuration.browserCapabilities = capabilities;
-
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @BeforeEach
